ae59e4360b11e0a6999948179e5e111235fb2c76
kusibehan12 <kusibehan@icloud.com>
2023-11-09 16:34:57 +0100

    First Commit

---
+++ weather-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { InputComponent } from "./components/main.tsx";
+
+function App() {
+  return (
+    <div className="App">
+      <InputComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-app/src/components/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function InputComponent(){
+    return (    
+    <div>
+        <input></input>
+    </div>)
+}
+
+export {InputComponent}
