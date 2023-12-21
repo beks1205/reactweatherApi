f2986329154141e2edb17dd0bdea8e670a3934aa
kusibehan12 <kusibehan@icloud.com>
2023-12-21 10:20:10 +0100

    Button component exposure

--- weather-app/src/components/button/button.tsx
+++ weather-app/src/components/button/button.tsx
@@ -1,11 +1,28 @@
 import React from "react";
 
+const UrlOpenWeather = "https://api.openweathermap.org/data/2.5/weather"
+
 function SubmitButton() {
+    doAsyncTask().then((data) => {
+        console.log(data);
+    })
     return (
         <div>
-            <button>buttonMock</button>
+            <button onClick={doAsyncTask}>Open Weather</button>
         </div>
     )
-}
+};
+
+async function doAsyncTask() {
+    const url = (
+      UrlOpenWeather +
+      new URLSearchParams({ "q": "1", "appid": "2173ae4a43c4f03c3a3ea3530ae341cc" }
+    ))
+  
+    const result = await fetch(url)
+      .then(response => response.json());
+  };
+
+
 
 export {SubmitButton}
